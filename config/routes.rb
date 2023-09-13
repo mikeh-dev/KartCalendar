0d33ff68139b511505615fa37a58c9d1849833be
mikeh-dev <mikeh112@hotmail.com>
2023-09-13 20:15:34 +0100

    Initial Commit - Using Rails UI

---
+++ config/routes.rb
@@ -0,0 +1,34 @@
+Rails.application.routes.draw do
+	get 'admin/integrations', to: 'page#integrations'
+	get 'admin/team', to: 'page#team'
+	get 'admin/billing', to: 'page#billing'
+	get 'admin/notifications', to: 'page#notifications'
+	get 'admin/settings', to: 'page#settings'
+	get 'admin/activity', to: 'page#activity'
+	get 'admin/profile', to: 'page#profile'
+	get 'admin/people', to: 'page#people'
+	get 'admin/calendar', to: 'page#calendar'
+	get 'admin/assignments', to: 'page#assignments'
+	get 'admin/message', to: 'page#message'
+	get 'admin/messages', to: 'page#messages'
+	get 'admin/project', to: 'page#project'
+	get 'admin/projects', to: 'page#projects'
+	get 'admin/dashboard', to: 'page#dashboard'
+	get 'pricing', to: 'page#pricing'
+	get 'about', to: 'page#about'
+	
+  if Rails.env.development? || Rails.env.test?
+    mount Railsui::Engine, at: "/railsui"
+  end
+
+  # Inherits from Railsui::PageController#index
+  # To overide, add your own page#index view or change to a new root
+  # Visit the start page for Rails UI any time at /railsui/start
+  root action: :index, controller: "railsui/page"
+
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
