91c4c789647cda71c1404fb632fcb9e362118f69
mikeh-dev <mikeh112@hotmail.com>
2023-09-25 22:20:28 +0100

    Events indew view added with placeholder content, events controller added, changed navlink to suit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   has_and_belongs_to_many :users, join_table: 'event_users'
   
-
+  def random_image_url
+    Faker::LoremFlickr.image(size: "300x200", search_terms: ["event"])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -31,4 +31,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+	get 'events', to: 'event#index'
 end
