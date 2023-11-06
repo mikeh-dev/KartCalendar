21276a61515231df91d2d43a54e36eaef6454e8d
mikeh-dev <mikeh112@hotmail.com>
2023-11-06 23:27:03 +0000

    Minor edit on champiopnship follwings controller to fix follow/unfollow error

--- app/controllers/championship_followings_controller.rb
+++ app/controllers/championship_followings_controller.rb
@@ -1,19 +1,16 @@
-# app/controllers/championship_followings_controller.rb
 class ChampionshipFollowingsController < ApplicationController
   before_action :authenticate_user!
 
   def create
     @championship = Championship.find(params[:championship_id])
     current_user.championships << @championship unless current_user.championships.include?(@championship)
-    # Optionally follow all events of the championship
     @championship.events.each { |event| current_user.events << event unless current_user.events.include?(event) }
     redirect_to championship_path(@championship)
   end
 
   def destroy
-    @championship = Championship.find(params[:id])
+    @championship = Championship.find(params[:championship_id])
     current_user.championships.delete(@championship)
-    # Optionally unfollow all events of the championship
     @championship.events.each { |event| current_user.events.delete(event) }
     redirect_to championship_path(@championship)
   end
