91207485e3869dd8795f939d1978d7d7d7458dc0
mikeh-dev <mikeh112@hotmail.com>
2023-11-14 00:25:39 +0000

    Deleted unneeded controllers and models

--- app/models/championship_following.rb
+++
@@ -1,4 +0,0 @@
-class ChampionshipFollowing < ApplicationRecord
-  belongs_to :user
-  belongs_to :championship
-end
--- app/models/track_following.rb
+++
@@ -1,4 +0,0 @@
-class TrackFollowing < ApplicationRecord
-  belongs_to :user
-  belongs_to :track
-end
