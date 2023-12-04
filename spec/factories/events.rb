100d9b3d7d389aa9ca2b6b771c075c3661e13ac2
mikeh-dev <mikeh112@hotmail.com>
2023-12-04 16:48:54 +0000

    Added more user_permisson tests and event controller logiv for testing admin for specific actions

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,5 +3,8 @@ FactoryBot.define do
     title { "Sample Event" }
     description { Faker::Lorem.paragraph }
     date { "1/1/24"}
+    price { rand(1000..5000) }
+    event_type { "Race" }
+    track
   end
 end
--- spec/system/user_permissions_spec.rb
+++ spec/system/user_permissions_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe 'User Permissions', type: :system do
   let(:non_admin) { FactoryBot.create(:user) }
   let(:track) { FactoryBot.create(:track) }
   let(:event) { FactoryBot.create(:event) }
+  let(:championship) { FactoryBot.create(:championship) }
+  let(:another_user) { FactoryBot.create(:user) }
 
   before do
     login_as(non_admin, scope: :user)
@@ -19,6 +21,8 @@ RSpec.describe 'User Permissions', type: :system do
     end
 
     it 'does not allow non-admin to edit a track' do
+      visit tracks_path
+      expect(page).not_to have_selector(:link_or_button, 'Edit')
       visit edit_track_path(track)
       expect(page).to have_current_path(root_path)
       expect(page).to have_content('You are not authorized to perform this action.')
@@ -29,9 +33,45 @@ RSpec.describe 'User Permissions', type: :system do
       expect(page).not_to have_selector(:link_or_button, 'Delete')
     end
 
-    it 'does not allow a non-admin user to see an Edit Track Button on track index' do
-      visit tracks_path
-      expect(page).not_to have_content("Edit track")
+    it 'does not allow non-admin to create an event' do
+      visit new_event_path
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_content('You are not authorized to perform this action.')
     end
+
+    it 'does not allow non-admin to edit an event' do
+      visit event_path(event)
+      expect(page).not_to have_selector(:link_or_button, 'Edit')
+      visit edit_event_path(event)
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_content('You are not authorized to perform this action.')
+    end
+
+    it 'does not allow non-admin to delete an event' do
+      visit event_path(event)
+      expect(page).not_to have_selector(:link_or_button, 'Delete')
+    end
+
+    it 'does not allow a non-admin user to create a championship' do
+      visit championships_path
+      expect(page).not_to have_selector(:link_or_button, 'New Championship')
+      visit new_championship_path
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_content('You are not authorized to perform this action.')
+    end
+
+    it 'does not allow a non-admin user to edit a championship' do
+      visit championship_path(championship)
+      expect(page).not_to have_selector(:link_or_button, 'Edit')
+      visit edit_championship_path(championship)
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_content('You are not authorized to perform this action.')
+    end
+
+    it 'does not allow a non-admin user to delete a championship' do
+      visit championship_path(championship)
+      expect(page).not_to have_selector(:link_or_button, 'Delete')
+    end
+
   end
 end
