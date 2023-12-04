8d14cb4ce75202125e5e5f5faab78f8d19a07100
mikeh-dev <mikeh112@hotmail.com>
2023-12-04 00:05:46 +0000

    Added user_profile_update spec

---
+++ spec/system/user_profile_update_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'User Profile Update', type: :system do
+  let(:user) { FactoryBot.create(:user) }
+
+  before do
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+  end
+
+  context 'when user is signed in' do
+    it 'allows user to update profile' do
+      visit root_path
+      find("#dropdowns-nav-toggle").click
+      find("#profile-button").click
+      click_link 'Edit account'
+      fill_in 'Email', with: 'test@email.com'
+      fill_in 'Current password', with: user.password
+      click_button 'Update'
+      expect(page).to have_content('Your account has been updated successfully.')
+      expect(user.reload.email).to eq('test@email.com')
+    end
+
+
+    it 'does not allow user to update profile with invalid email' do
+      visit root_path
+      find("#dropdowns-nav-toggle").click
+      find("#profile-button").click
+      click_link 'Edit account'
+      fill_in 'Email', with: 'test@email.com'
+      fill_in 'Current password', with: 'wrong_password'
+      click_button 'Update'
+      expect(page).to have_content('Current password is invalid')
+    end
+  end
+
+
+end
