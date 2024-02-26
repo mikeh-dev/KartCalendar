38023225c3ffbc8fd2772f816c8dade62229c19d
mikeh-dev <mikeh112@hotmail.com>
2024-02-26 13:21:29 +0000

    Sorted the current day highlight

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,5 +2,7 @@ class CalendarsController < ApplicationController
   def show
     date = Date.parse(params[:date])
     render partial: 'calendars/calendar', locals: { anchor_date: date }
+    puts "Anchor date: #{date}"
+
   end
 end
