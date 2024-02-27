5256d4dfa29c6cc478033650cffc7655797bd693
mikeh-dev <mikeh112@hotmail.com>
2024-02-27 00:18:03 +0000

    Fixed the calendar event details link to event show page

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -12,20 +12,18 @@ export default class extends Controller {
     console.log(event)
     this.clearDayTargets()
     if (event.target.tagName == "TIME") {
-      event.target.classList.add("bg-white", "font-semibold", "text-black")
+      event.target.classList.add("bg-white")
       console.log(event.target)
     } else if (event.target.tagName == "DIV") {
-      event.target.parentElement.classList.add("bg-white", "font-semibold", "text-black")
+      event.target.parentElement.classList.add("bg-white")
     } else {
-      event.target.querySelector("time").classList.add("bg-white", "font-semibold", "text-black")
+      event.target.querySelector("time").classList.add("bg-white")
     }
   }
 
   clearDayTargets() {
     this.dayTargets.forEach(dayButton => {
       dayButton.classList.remove("bg-white")
-      dayButton.classList.remove("font-semibold")
-      dayButton.classList.remove("text-black")
     })
   }
 }
