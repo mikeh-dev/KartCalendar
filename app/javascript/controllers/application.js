ca3a438e27743598f90b076e256b9c77254a5a0b
mikeh-dev <mikeh112@hotmail.com>
2024-03-22 11:54:56 +0000

    Changed GA approach

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,10 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+document.addEventListener("turbo:load", function(event) {
+  window.dataLayer = window.dataLayer || []
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date())
+  gtag('config', 'G-2Q5VPDS4LY', {'page_location': event.detail.url})
+}, false)
