0cf8bbed840223a631d6a531e373df114aae649f
mikeh-dev <mikeh112@hotmail.com>
2024-03-26 21:33:51 +0000

    Added service record factory bot

--- app/models/service_record.rb
+++ app/models/service_record.rb
@@ -1,10 +1,29 @@
-class ServiceRecord < ApplicationRecord
-  belongs_to :user
-  belongs_to :engine
+FactoryBot.define do
+  factory :service_record do
+    association :user
+    association :engine
+    date { Date.today }
+    description { "Regular service and maintenance." }
+    engine_builder { "Top Engine Builders Inc." }
+    notes { "Everything checked and verified. Ready for the next race." }
 
-  has_one_attached :invoice
-  has_many_attached :dyno_sheets
+    after(:build) do |service_record|
+      # Adjust paths and filenames as necessary
+      invoice_path = Rails.root.join('spec', 'support', 'files', 'sample_invoice.pdf')
+      dyno_sheet_path = Rails.root.join('spec', 'support', 'files', 'sample_dyno_sheet.jpg')
 
-  validates :date, :description, presence: true
+      service_record.invoice.attach(
+        io: File.open(invoice_path),
+        filename: 'sample_invoice.pdf',
+        content_type: 'application/pdf'
+      )
 
+      service_record.dyno_sheets.attach(
+        io: File.open(dyno_sheet_path),
+        filename: 'sample_dyno_sheet.jpg',
+        content_type: 'image/jpeg'
+      )
+      # Attach more dyno_sheets if necessary
+    end
+  end
 end
---
+++ spec/factories/servicerecord.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+
+  factory :servicerecord do
+    
+  end
+end
