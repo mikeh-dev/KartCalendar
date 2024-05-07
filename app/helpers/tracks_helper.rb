8f10cc07b2f27ea57c916b5abe0316d5204d7a50
mikeh-dev <mikeh112@hotmail.com>
2024-05-07 22:03:49 +0100

    Cleaned up track index view, increase map size, green tests

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -9,9 +9,14 @@ module TracksHelper
       safe_join([
         image_tag(img_url, alt: "#{track.name} image", class: ""),
         tag.h3(track.name, class: "text-lg font-bold text-gray-900 mt-2"),
-        tag.p(track.location, class: "text-sm text-gray-700"),
-        tag.p(track.contact_number, class: "text-sm text-gray-700"),
-        tag.p("Next Race Meeting:", class: "text-sm text-gray-700 mt-2"),
+        tag.div(class: "flex items-center") do
+          safe_join([
+            image_tag('icons/outline/map-pin.svg', width: '24', height: '24', class: 'mr-2'),
+            tag.p(track.location, class: "text-sm text-gray-700")
+          ])
+        end,
+        tag.p(tag.strong("Phone:") + " #{track.contact_number}", class: "text-sm text-gray-700"),
+        tag.p("Next Race Meeting:", class: "text-sm text-gray-700 mt-2 font-bold"),
         tag.p(" #{next_race_meeting_date}", class: "text-sm text-gray-700"),
 
         link_to('View Track', track_path(track), class: "btn btn-primary mt-2 w-full text-center")
