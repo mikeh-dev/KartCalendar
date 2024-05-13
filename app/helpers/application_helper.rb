97e65422760a7deb4907fc246a1d0c42a9150925
mikeh-dev <mikeh112@hotmail.com>
2024-05-13 15:36:23 +0100

    cleaned up view event button event index, moved svg and button styling to partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
     images = Dir.glob('random/*', base: Rails.root.join('app', 'assets', 'images'))
     ActionController::Base.helpers.asset_path(images.sample)
   end
+
+  def svg_icon
+    raw('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" class="w-4 h-4">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"></path>
+        </svg>')
+  end
 end
