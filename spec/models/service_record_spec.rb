40da53ac704ac2e3cf16c8faab53caf35dfae07e
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-08-09 12:06:58 +0100

    Fixed model specs for service records

--- spec/models/service_record_spec.rb
+++ spec/models/service_record_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe ServiceRecord, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:date) }
-    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:engine_builder) }
+    it { is_expected.to validate_presence_of(:new_seal_number) }
+    
   end
 
   describe 'associations' do
