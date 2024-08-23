a768e7d80fd2a899e35fd49262d419fe5a94f484
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-08-23 10:31:52 +0100

    cleaned up calendar events partial

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 	get 'cookie', to: 'page#cookie'
 	get 'new_dash', to: 'dashboard#new_dash'
 
+	get 'calendar', to: 'page#calendar'
+
 
 	resources :dashboard, only: [:index] do
 		collection do
