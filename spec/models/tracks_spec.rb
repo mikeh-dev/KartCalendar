926982953aa5285368c4dd49f08b516256446c05
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-09-30 12:54:52 +0100

    Green tests again after to fixing the issue in model spec

--- spec/models/tracks_spec.rb
+++ spec/models/tracks_spec.rb
@@ -15,7 +15,6 @@ RSpec.describe Track, type: :model do
   end
 
   describe "validations" do
-    it { should validate_presence_of(:name) }
     it { should validate_presence_of(:address) }
     it { should validate_presence_of(:contact_number) }
     it { should validate_presence_of(:length) }
