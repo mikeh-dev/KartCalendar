c8a14d3446b0ccb464a2ff6debe970454d9ad112
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-10-01 18:30:10 +0100

    Fixed a little styling issue on track index

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,12 +2,8 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["input"];
-  position = 0;
 
   connect() {
-    // Save cursor position before Turbo renders new content and restore it after
-    document.addEventListener('turbo:before-render', this.saveCursorPosition.bind(this));
-    document.addEventListener('turbo:render', this.restoreCursorPosition.bind(this));
   }
 
   submit() {
@@ -18,24 +14,8 @@ export default class extends Controller {
   }
 
   inputChanged(event) {
-    // Automatically submit when input is cleared
     if (event.target.value === "") {
       this.submit();
-    }
-  }
-
-  saveCursorPosition() {
-    // Save the current cursor position in the input field
-    if (this.inputTarget) {
-      this.position = this.inputTarget.selectionStart;
-    }
-  }
-
-  restoreCursorPosition() {
-    // Restore the saved cursor position and refocus the input
-    if (this.inputTarget && this.position !== null) {
-      this.inputTarget.setSelectionRange(this.position, this.position);
-      this.inputTarget.focus();
     }
   }
 }
