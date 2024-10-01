2b90c7b24a07b93f16310c9b8114880bbe15c6bf
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-10-01 20:58:16 +0100

    Corrected the cursor issue on tracks index form

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,21 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["input"];
 
   connect() {
   }
 
   submit() {
-    clearTimeout(this.timeout); // Clear any previous timeouts
+    clearTimeout(this.timeout); 
     this.timeout = setTimeout(() => {
-      this.element.requestSubmit(); // Submit the form after a 300ms debounce
+      this.element.requestSubmit();
     }, 300);
-  }
-
-  inputChanged(event) {
-    if (event.target.value === "") {
-      this.submit();
-    }
   }
 }
