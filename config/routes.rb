d4039cb69704efa1c0a01ccdb4b6dd573abf001b
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-10-01 10:28:34 +0100

    Routes file tidy up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  
+
+	root action: :home, controller: "page"
+
 	get 'privacy', to: 'page#privacy'
 	get 'about', to: 'page#about'
 	get 'home', to: 'page#home'
@@ -17,21 +19,10 @@ Rails.application.routes.draw do
 				delete :remove_engine_service_record_image, to: 'service_records#remove_image', as: :remove_image
 			end
 		end
-		
 		member do
 			delete :remove_image
 		end
 	end
-
-	resources :contacts, only: [:new, :create]
-	
-  if Rails.env.development? || Rails.env.test?
-    mount Railsui::Engine, at: "/railsui"
-  end
-
-  root action: :home, controller: "page"
-
-  devise_for :users
 
 	resources :events do
 		collection do
@@ -40,14 +31,20 @@ Rails.application.routes.draw do
 			get :check_test
 		end
 	end
-	
+
+	devise_for :users
+	resources :contacts, only: [:new, :create]
 	resources :tracks
 	resources :championships
 	resource :calendars, only: [:show]
 	resources :follows, only: [:create, :destroy]
 
 	get '*unmatched_route', to: 'application#not_found', constraints: lambda { |req|
-  req.path.exclude? 'rails/active_storage'
+		req.path.exclude? 'rails/active_storage'
 	}
+
+	if Rails.env.development? || Rails.env.test?
+    mount Railsui::Engine, at: "/railsui"
+  end
 
 end
