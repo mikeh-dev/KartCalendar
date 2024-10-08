733b6866546e95a1eeea878540217761353079ce
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-10-08 16:39:46 +0100

    Little clean up on champ model'

--- app/models/championship.rb
+++ app/models/championship.rb
@@ -3,16 +3,13 @@ class Championship < ApplicationRecord
   has_one_attached :image
   has_one_attached :logo
   has_and_belongs_to_many :categories
-
+  has_many :follows, as: :followable
   belongs_to :user, optional: true
+  belongs_to :home_track, class_name: 'Track', foreign_key: 'home_track_id', optional: true
 
   validates :name, :description, :short_name, :long_name, presence: true
-
-  belongs_to :home_track, class_name: 'Track', foreign_key: 'home_track_id', optional: true
   validates :home_track_id, presence: true, if: -> { champ_type == 'Club' }
 
-  has_many :follows, as: :followable
-  
   store :social_media, accessors: [ :facebook, :instagram ]
 
   def next_champ_event
