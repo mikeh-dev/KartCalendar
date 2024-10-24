b5c3fb347b8e127e09f75733e49735430b34f9cb
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-10-24 22:06:56 +0100

    Attempted to implement a simpler role check approach with string inquirer but didn't work so reverted back

--- db/schema.rb
+++ db/schema.rb
@@ -225,10 +225,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_24_202351) do
     t.datetime "remember_created_at"
     t.string "first_name"
     t.string "last_name"
-    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- spec/system/champ_auth_spec.rb
+++ spec/system/champ_auth_spec.rb
@@ -7,23 +7,10 @@ RSpec.describe "ChampionshipAuth", type: :system do
   let(:manager) { create(:user, role: 'manager') }
   let(:championship) { create(:championship, user: manager) }
 
-  context 'as a regular user' do
-    before do 
-      login_as user
-    end
-
-    it 'denies access to edit any championships' do
-
-    end
-
-  end
-
-
   context 'as an admin user' do
     before do
       login_as admin
     end
-
   end
 
   context 'as a manager user' do
@@ -31,7 +18,5 @@ RSpec.describe "ChampionshipAuth", type: :system do
       login_as manager
       championship
     end
-
-
   end
 end
